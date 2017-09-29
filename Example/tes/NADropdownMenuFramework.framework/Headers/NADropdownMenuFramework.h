edbb3dea218f1aeaaec067c178fa7c0c715213ed
Yuriy Holovatskyi <yuriy.holovatskyi@thinkmobiles.com>
2017-09-29 12:28:43 +0300

    * minor refactor

--- Example/tes/NADropdownMenuFramework.framework/Headers/NADropdownMenuFramework.h
+++ Example/tes/NADropdownMenuFramework.framework/Headers/NADropdownMenuFramework.h
@@ -16,5 +16,5 @@ FOUNDATION_EXPORT const unsigned char NADropdownMenuFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <NADropdownMenuFramework/PublicHeader.h>
 
-#import "UIColor+MKDropdownMenu.h"
-#import "MKDropdownMenu.h"
+#import <NADropdownMenuFramework/UIColor+MKDropdownMenu.h>
+#import <NADropdownMenuFramework/MKDropdownMenu.h>
